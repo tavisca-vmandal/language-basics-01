c0f819fe32e31b54be0db12eea246998f4de96f3
tavisca-vmandal <51911919+tavisca-vmandal@users.noreply.github.com>
2019-06-27 14:13:44 +0530

    followed naming convention

--- Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
@@ -21,52 +21,78 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
             var result = FindDigit(args).Equals(expected) ? "PASS" : "FAIL";
             Console.WriteLine($"{args} : {result}");
         }
-        public static int Calculation(string s_num1,string s_num2,string s_num,int flag)
+        public static int GetDigit(string firstNumber,string secondNumber,string resultantNumber,string flag)
         {
-            int num1,num2;
-            int temp,j;
-            string s_temp;
-            num1=Int32.Parse(s_num1);
-            num2=Int32.Parse(s_num2);
-            if(flag==0)
-                temp=num1*num2;
-            else
-            {
-                if(num2%num1!=0||num1==0||num2==0)
-                    return -1;
-                temp=num2/num1;
-            }
-             s_temp=temp.ToString();
-             if(s_temp.Length==s_num.Length)
-             {
-                j=s_num.IndexOf('?');
-                return s_temp[j]-'0';
-             }
-             else
+            int firstNumberInt=Int32.Parse(firstNumber);//convert string numbers into integer
+            int secondNumberInt=Int32.Parse(secondNumber);
+
+            if(firstNumberInt==0||secondNumberInt==0)//corner case if any of the numbers is zero
                 return -1;
+                
+            int result=0;
+            int index=0;
+
+            if(flag=="multiply")
+                result=firstNumberInt*secondNumberInt;//multiply the numbers to find RHS of given Equation
+            else if(flag=="divide")
+            {
+                if(secondNumberInt%firstNumberInt!=0)//corner case..checks if the numbers is divisible
+                    return -1;
+                
+                result=secondNumberInt/firstNumberInt;//divide the numbers to find LHS of given Equation
+            }
+            else
+                Console.WriteLine("Invalid operation");
+
+            string resultString=result.ToString();
+
+            if(resultString.Length==resultantNumber.Length)//compare the length of calculated result and given result
+            {
+                index=resultantNumber.IndexOf('?');
+                return resultString[index]-'0';
+            }
+            else
+                return -1;
+        }
+        
+        public static string[] SplitEquation(string equation)
+        {
+            string[] splitByStar=equation.Split("*");//split by * into array of strings
+            string A=splitByStar[0];
+            string[] splitByEqual=splitByStar[1].Split("=");//split the string by =
+            string B=splitByEqual[0];
+            string C=splitByEqual[1];
+
+            string[] splittedEquation=new string[3];
+            splittedEquation[0]=A;
+            splittedEquation[1]=B;
+            splittedEquation[2]=C;
+
+            return splittedEquation;
         }
         public static int FindDigit(string equation)
         {
-            string[] arrayABC=equation.Split("*");
-            string A=arrayABC[0];
-            string[] arrayBC=arrayABC[1].Split("=");
-            string B=arrayBC[0];
-            string R=arrayBC[1];
-            int m_digit;
+            int missingDigit;
+            string[] splittedEquation = SplitEquation(equation);//split the given equation into three different strings
+            
+            string A=splittedEquation[0];
+            string B=splittedEquation[1];
+            string C=splittedEquation[2];
+
             if(A.Contains("?"))
             {
-                m_digit=Calculation(B,R,A,1);
+                missingDigit=GetDigit(B,C,A,"divide");
                 
             }
             else if(B.Contains("?"))
             {
-                m_digit=Calculation(A,R,B,1);
+                missingDigit=GetDigit(A,C,B,"divide");
             }      
             else
             {
-                m_digit=Calculation(A,B,R, 0);
+                missingDigit=GetDigit(A,B,C,"multiply");
             }
-            return m_digit;
+            return missingDigit;
         }
     }
 }
